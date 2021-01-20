d42a44c09d1d32981573ec9f23e551723361786c
mimaki <hiroshi.mimaki@gmail.com>
2021-01-20 17:00:03 +0900

    Add 06mrbgem.

---
+++ src/06mrbgem/host-sleep.rb
@@ -0,0 +1,8 @@
+MRuby::Build.new do |conf|
+  conf.toolchain
+
+  # include the default GEMs
+  conf.gembox 'default'
+  # Use mrbgems
+  conf.gem :git => 'https://github.com/matsumotory/mruby-sleep.git'
+end
