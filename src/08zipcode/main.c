9ecc04cb18f50bc2f11a07e37ff970d19001ab5a
mimaki <hiroshi.mimaki@gmail.com>
2021-01-21 17:24:41 +0900

    Refine 08zipcode.

--- src/08zipcode/main.c
+++ src/08zipcode/main.c
@@ -3,23 +3,29 @@
 #include "mruby/variable.h"
 #include "mruby/dump.h"
 
-int main(int argc, char *argv[])
+static void set_args(mrb_state *mrb, int argc, char *argv[])
 {
-  mrb_state *mrb = mrb_open();
-  mrb_value args;
-  int i;
-  extern const uint8_t appbin[];
-
   /* set 'ARGV[]' */
-  args = mrb_ary_new_capa(mrb, argc - 1);
-  for (i=1; i<argc; i++) {
+  mrb_value args = mrb_ary_new_capa(mrb, argc - 1);
+  for (int i=1; i<argc; i++) {
     mrb_ary_push(mrb, args, mrb_str_new_cstr(mrb, argv[i]));
   }
   mrb_define_global_const(mrb, "ARGV", args);
+
   /* set '$0' */
   mrb_gv_set(mrb, mrb_intern_lit(mrb, "$0"), mrb_str_new_cstr(mrb, argv[0]));
+}
+
+int main(int argc, char *argv[])
+{
+  mrb_state *mrb = mrb_open();
+  extern const uint8_t appbin[];
+
+  /* set 'ARGV[]' &'$0' */
+  set_args(mrb, argc, argv);
 
   mrb_load_irep(mrb, appbin);
+  
   mrb_close(mrb);
   return 0;
 }
